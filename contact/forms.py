8a26405567e82ad7194006c37fe3d8b22f7bff41
Lera-B-E <bondareva.valeria2011@yandex.ru>
2022-12-09 23:54:05 +0300

    website

---
+++ contact/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ("email", )
+        widgets = {
+            "email": forms.TextInput(attrs={"class": "subscribe__input", "placeholder": "Your Email . . ."})
+        }
+        labels = {
+            "email": ''
+        }
